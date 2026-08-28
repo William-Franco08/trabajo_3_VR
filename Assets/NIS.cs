26f474d8bf72e976b9c64380dec07abf64df9e74
William-Franco08 <flw5856@gmail.com>
2026-08-28 02:25:29 -0500

    trabajo gojo

--- Assets/NIS.cs
+++ Assets/NIS.cs
@@ -109,6 +109,33 @@ public partial class @NIS: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""151bfb22-c4c2-4921-9c8d-8bbb8aae966f"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Crouch"",
+                    ""type"": ""Button"",
+                    ""id"": ""593777c5-46e4-49d9-96e6-4e3e4847b863"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Dance"",
+                    ""type"": ""Button"",
+                    ""id"": ""d4f9f532-894a-4efc-a819-94e75ba6276c"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -184,7 +211,7 @@ public partial class @NIS: IInputActionCollection2, IDisposable
                     ""path"": ""<Keyboard>/space"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": "";Teclado"",
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
@@ -195,8 +222,74 @@ public partial class @NIS: IInputActionCollection2, IDisposable
                     ""path"": ""<Gamepad>/buttonSouth"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": """",
+                    ""groups"": "";Control"",
                     ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""df1f484a-4505-410a-b09f-5ab942343a88"",
+                    ""path"": ""<Gamepad>/leftTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f8dde237-f625-4357-be5b-4ae755752faf"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Teclado"",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c397d111-a608-4350-8ae5-721d693105fc"",
+                    ""path"": ""<Keyboard>/ctrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Teclado"",
+                    ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""360a0c2e-f6c4-4bd4-b937-fd5f388d94e9"",
+                    ""path"": ""<Gamepad>/leftStickPress"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""803edd7b-796c-4672-8a9d-0f5613f81510"",
+                    ""path"": ""<Keyboard>/tab"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dance"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f7bd239d-6f94-460b-89b7-fd861fa8973a"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dance"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -232,6 +325,9 @@ public partial class @NIS: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Run = m_Player.FindAction("Run", throwIfNotFound: true);
+        m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
+        m_Player_Dance = m_Player.FindAction("Dance", throwIfNotFound: true);
     }
 
     ~@NIS()
@@ -314,6 +410,9 @@ public partial class @NIS: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Run;
+    private readonly InputAction m_Player_Crouch;
+    private readonly InputAction m_Player_Dance;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -333,6 +432,18 @@ public partial class @NIS: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Jump".
         /// </summary>
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Run".
+        /// </summary>
+        public InputAction @Run => m_Wrapper.m_Player_Run;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Crouch".
+        /// </summary>
+        public InputAction @Crouch => m_Wrapper.m_Player_Crouch;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Dance".
+        /// </summary>
+        public InputAction @Dance => m_Wrapper.m_Player_Dance;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -365,6 +476,15 @@ public partial class @NIS: IInputActionCollection2, IDisposable
             @Jump.started += instance.OnJump;
             @Jump.performed += instance.OnJump;
             @Jump.canceled += instance.OnJump;
+            @Run.started += instance.OnRun;
+            @Run.performed += instance.OnRun;
+            @Run.canceled += instance.OnRun;
+            @Crouch.started += instance.OnCrouch;
+            @Crouch.performed += instance.OnCrouch;
+            @Crouch.canceled += instance.OnCrouch;
+            @Dance.started += instance.OnDance;
+            @Dance.performed += instance.OnDance;
+            @Dance.canceled += instance.OnDance;
         }
 
         /// <summary>
@@ -382,6 +502,15 @@ public partial class @NIS: IInputActionCollection2, IDisposable
             @Jump.started -= instance.OnJump;
             @Jump.performed -= instance.OnJump;
             @Jump.canceled -= instance.OnJump;
+            @Run.started -= instance.OnRun;
+            @Run.performed -= instance.OnRun;
+            @Run.canceled -= instance.OnRun;
+            @Crouch.started -= instance.OnCrouch;
+            @Crouch.performed -= instance.OnCrouch;
+            @Crouch.canceled -= instance.OnCrouch;
+            @Dance.started -= instance.OnDance;
+            @Dance.performed -= instance.OnDance;
+            @Dance.canceled -= instance.OnDance;
         }
 
         /// <summary>
@@ -462,5 +591,26 @@ public partial class @NIS: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnJump(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Run" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnRun(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Crouch" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnCrouch(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Dance" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnDance(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -7,25 +7,52 @@ public class Movimiento : MonoBehaviour
 
     private Vector2 moveInput;
 
-    public float speed = 5f;
+    [Header("Movimiento")]
+    public float velocidadCaminar = 3f;
+    public float velocidadCorrer = 6f;
+    public float velocidadAgachado = 1.5f;
+    public float velocidadGiro = 10f;
+
+    [Header("Salto")]
     public float fuerzaSalto = 5f;
 
+    [Header("Estado")]
+    private bool estaCorriendo = false;
+    private bool estaAgachado = false;
+    private bool estaBailando = false;
+    private bool estaEnSuelo = true;
+
     private Rigidbody rb;
+    private Animator animator;
 
     private void Awake()
     {
         inputActions = new NIS();
+
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
         inputActions.Player.Enable();
 
+        // Movimiento
         inputActions.Player.Move.performed += OnMove;
         inputActions.Player.Move.canceled += OnMove;
 
+        // Salto
         inputActions.Player.Jump.performed += OnJump;
+
+        // Correr
+        inputActions.Player.Run.performed += OnRun;
+        inputActions.Player.Run.canceled += OnRun;
+
+        // Agacharse
+        inputActions.Player.Crouch.performed += OnCrouch;
+
+        // Bailar
+        inputActions.Player.Dance.performed += OnDance;
     }
 
     private void OnDisable()
@@ -35,25 +62,228 @@ public class Movimiento : MonoBehaviour
 
         inputActions.Player.Jump.performed -= OnJump;
 
+        inputActions.Player.Run.performed -= OnRun;
+        inputActions.Player.Run.canceled -= OnRun;
+
+        inputActions.Player.Crouch.performed -= OnCrouch;
+
+        inputActions.Player.Dance.performed -= OnDance;
+
         inputActions.Player.Disable();
     }
+
+    // =====================================================
+    // MOVIMIENTO
+    // =====================================================
 
     private void OnMove(InputAction.CallbackContext value)
     {
         moveInput = value.ReadValue<Vector2>();
     }
 
-    private void OnJump(InputAction.CallbackContext context)
-    {
-        Debug.Log("Saltar");
-
-        rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
-    }
-
     private void Update()
     {
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y);
+        Vector3 movimiento = new Vector3(
+            moveInput.x,
+            0f,
+            moveInput.y
+        );
 
-        transform.Translate(movement * speed * Time.deltaTime);
+        // =================================================
+        // BAILE
+        // =================================================
+
+        // Si está bailando, no se mueve
+        if (!estaBailando)
+        {
+            // =================================================
+            // GIRAR Y MOVER
+            // =================================================
+
+            if (movimiento.magnitude > 0.1f)
+            {
+                // Dirección hacia donde queremos mirar
+                Quaternion rotacionObjetivo =
+                    Quaternion.LookRotation(movimiento);
+
+                // Girar suavemente
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    rotacionObjetivo,
+                    velocidadGiro * Time.deltaTime
+                );
+
+                // =================================================
+                // VELOCIDAD
+                // =================================================
+
+                float velocidadActual;
+
+                if (estaAgachado)
+                {
+                    // Agachado
+                    velocidadActual = velocidadAgachado;
+                }
+                else if (estaCorriendo)
+                {
+                    // Corriendo
+                    velocidadActual = velocidadCorrer;
+                }
+                else
+                {
+                    // Caminando
+                    velocidadActual = velocidadCaminar;
+                }
+
+                // =================================================
+                // MOVIMIENTO HACIA ADELANTE
+                // =================================================
+
+                transform.Translate(
+                    Vector3.forward *
+                    velocidadActual *
+                    Time.deltaTime
+                );
+            }
+        }
+
+        // =================================================
+        // ANIMATOR
+        // =================================================
+
+        float velocidad = moveInput.magnitude;
+
+        animator.SetFloat("Speed", velocidad);
+
+        animator.SetBool(
+            "IsRunning",
+            estaCorriendo
+        );
+
+        animator.SetBool(
+            "IsCrouching",
+            estaAgachado
+        );
+
+        animator.SetBool(
+            "IsGrounded",
+            estaEnSuelo
+        );
+
+        animator.SetBool(
+            "Dance",
+            estaBailando
+        );
+    }
+
+    // =====================================================
+    // CORRER
+    // =====================================================
+
+    private void OnRun(InputAction.CallbackContext context)
+    {
+        estaCorriendo = context.ReadValueAsButton();
+
+        // No puede correr mientras está agachado
+        if (estaAgachado)
+        {
+            estaCorriendo = false;
+        }
+
+        // No puede correr mientras baila
+        if (estaBailando)
+        {
+            estaCorriendo = false;
+        }
+    }
+
+    // =====================================================
+    // AGACHARSE
+    // =====================================================
+
+    private void OnCrouch(InputAction.CallbackContext context)
+    {
+        // Si está bailando, no puede agacharse
+        if (estaBailando)
+            return;
+
+        // Alternar agachado / levantado
+        estaAgachado = !estaAgachado;
+
+        // Si se agacha, deja de correr
+        if (estaAgachado)
+        {
+            estaCorriendo = false;
+        }
+    }
+
+    // =====================================================
+    // SALTAR
+    // =====================================================
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        // No puede saltar si ya está en el aire
+        if (!estaEnSuelo)
+            return;
+
+        // No puede saltar agachado
+        if (estaAgachado)
+            return;
+
+        // No puede saltar bailando
+        if (estaBailando)
+            return;
+
+        Debug.Log("Saltar");
+
+        rb.AddForce(
+            Vector3.up * fuerzaSalto,
+            ForceMode.Impulse
+        );
+
+        estaEnSuelo = false;
+
+        animator.SetBool(
+            "IsGrounded",
+            false
+        );
+    }
+
+    // =====================================================
+    // BAILAR
+    // =====================================================
+
+    private void OnDance(InputAction.CallbackContext context)
+    {
+        // Alternar baile
+        estaBailando = !estaBailando;
+
+        if (estaBailando)
+        {
+            // Al empezar a bailar:
+            estaCorriendo = false;
+            estaAgachado = false;
+
+            // Dejar de moverse
+            moveInput = Vector2.zero;
+        }
+    }
+
+    // =====================================================
+    // DETECTAR SUELO
+    // =====================================================
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            estaEnSuelo = true;
+
+            animator.SetBool(
+                "IsGrounded",
+                true
+            );
+        }
     }
 }
